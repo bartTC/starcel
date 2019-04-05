fefe21fbb053409077c7e39870789e5770196d7d
Martin Mahner <martin@mahner.org>
2019-04-05 21:46:34 +0200

    Typos

--- posthtml.config.js
+++ posthtml.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   plugins: {
 
-    // Automatically add `alt=""` to all images which don'thave an alt
+    // Automatically add `alt=""` to all images which don't have an alt
     // attribute yet.
     'posthtml-alt-always': {},
 
     // Automatically add `width="<actual size>" height="<actual size>"` to all
-    // images which have`width="auto" height="auto"` set as an attribute.
+    // images which have `width="auto" height="auto"` set as an attribute.
     'posthtml-img-autosize': {
       'root': './src',
     },
